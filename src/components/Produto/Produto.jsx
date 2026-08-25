f9ef38ea35939253c7df96a15127ed716a3f473d
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2026-08-25 00:33:16 -0300

    alterando para produtos

--- src/components/Produto/Produto.jsx
+++ src/components/Produto/Produto.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import "./Produto.css";
 
 const Produto = (props) => {
-
-    return (
-        <div className="produto">
-            <div className="cabecalho">
-                <img src={props.imagem} alt={props.nome} />
-            </div>
-            <div className="rodape">
-                <h4>{props.nome} - {props.marca}</h4>
-                <h4>{props.preco}</h4>
-                <h5>{props.conservacao}</h5>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="produto">
+      <div className="cabecalho">
+        <img src={props.imagem} alt={props.nome} />
+      </div>
+      <div className="rodape">
+        <h4>
+          {props.nome} - {props.marca}
+        </h4>
+        <h4>R$ {props.preco}</h4>
+        <h5>{props.conservacao}</h5>
+      </div>
+    </div>
+  );
+};
 export default Produto;
-
